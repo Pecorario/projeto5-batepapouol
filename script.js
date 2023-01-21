aff2419429f252c9b9ab5992de5c5d7246eaf8e5
Taynara Pecorario <taynara_rp22@hotmail.com>
2023-01-21 07:50:01 -0300

    :sparkles: add right drawer and send message functions

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let messages = [];
+let users = [];
+let to = 'Todos';
+let visibility = 'público';
 let nickname, idTimerMessages, idTimerDwellTime;
 
 const createTimerToLoadMessages = () => {
@@ -22,6 +25,132 @@ const stillHere = async () => {
     nickname = '';
     goToLogin();
   }
+};
+
+const sendMessage = async () => {
+  try {
+    const message = document.querySelector('.input-message').value;
+
+    const newMessage = {
+      from: nickname,
+      to: to,
+      text: message,
+      type: visibility === 'público' ? 'message' : 'private_message'
+    };
+
+    await axios.post(
+      'https://mock-api.driven.com.br/api/v6/uol/messages',
+      newMessage
+    );
+
+    loadMessages();
+
+    document.querySelector('.input-message').value = '';
+  } catch (error) {
+    document.querySelector('.input-message').value = '';
+    console.log(error);
+  }
+};
+
+const selectVisibility = (content, type) => {
+  const selectedVisibility = document.querySelector('.visibility li.selected');
+  const check = document.querySelector('.visibility .container-check');
+
+  if (selectedVisibility !== null) {
+    selectedVisibility.classList.remove('selected');
+    check.remove();
+  }
+
+  const spanMessage = document.querySelector('.recipient');
+  spanMessage.innerText = `Enviando para ${to} (${type})`;
+  visibility = type;
+
+  content.classList.add('selected');
+  content.innerHTML += `
+    <div class="container-check" data-test="check">
+      <ion-icon name="checkmark-sharp" class="icon-check"></ion-icon>
+    </div>
+  `;
+};
+
+const selectTo = (content, receiver) => {
+  const selectedPerson = document.querySelector('.people li.selected');
+  const check = document.querySelector('.people .container-check');
+
+  if (selectedPerson !== null) {
+    selectedPerson.classList.remove('selected');
+    check.remove();
+  }
+
+  const spanMessage = document.querySelector('.recipient');
+  spanMessage.innerText = `Enviando para ${receiver} (${visibility})`;
+  to = receiver;
+
+  content.classList.add('selected');
+  content.innerHTML += `
+    <div class="container-check" data-test="check">
+      <ion-icon name="checkmark-sharp" class="icon-check"></ion-icon>
+    </div>
+  `;
+};
+
+const createListOfUsers = () => {
+  const listOfUsers = document.querySelector('.people');
+
+  listOfUsers.innerHTML = `
+    <li data-test="all" onclick="selectTo(this, 'Todos')" class="selected" >
+      <ion-icon name="people" class="icon-people"></ion-icon>Todos
+      <div class="container-check" data-test="check">
+        <ion-icon name="checkmark-sharp" class="icon-check"></ion-icon>
+      </div>
+    </li>
+  `;
+
+  users.map(
+    user =>
+      (listOfUsers.innerHTML += `
+        <li data-test="participant" onclick="selectTo(this, '${user.name}')">
+          <ion-icon name="person-circle" class="icon-person"></ion-icon>${user.name}
+        </li>
+      `)
+  );
+};
+
+const loadUsers = async () => {
+  try {
+    const response = await axios.get(
+      'https://mock-api.driven.com.br/api/v6/uol/participants'
+    );
+
+    users = response.data;
+
+    createListOfUsers();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const openRightDrawer = () => {
+  loadUsers();
+
+  const rightDrawer = document.querySelector('.right-drawer');
+  const rightDrawerBackground = document.querySelector(
+    '.right-drawer .background'
+  );
+  const rightDrawerContent = document.querySelector('.right-drawer .content');
+  const page = document.querySelector('.page-chat');
+
+  rightDrawer.classList.remove('hidden');
+  rightDrawerContent.style.width = '69.06vw';
+  page.style.overflow = 'hidden';
+};
+
+const closeRightDrawer = () => {
+  const rightDrawer = document.querySelector('.right-drawer');
+  const page = document.querySelector('.page-chat');
+
+  rightDrawer.classList.add('hidden');
+  page.style.overflow = 'auto';
 };
 
 const addLoader = () => {
@@ -75,10 +204,12 @@ const goToLogin = () => {
 const createMessages = () => {
   const messagesList = document.querySelector('main');
 
+  messagesList.innerHTML = ``;
+
   messages.map(message => {
     if (
-      (message.type === 'private_message' && message.from === nickname) ||
-      message.to === nickname
+      message.type === 'private_message' &&
+      (message.from === nickname || message.to === nickname)
     ) {
       return (messagesList.innerHTML += `
         <div class="message privately" data-test="message">
@@ -97,7 +228,7 @@ const createMessages = () => {
     return (messagesList.innerHTML += `
     <div class="message" data-test="message">
       <span>(${message.time})</span>&nbsp<strong>${message.from}</strong> para
-      <strong>${message.text}:</strong>&nbsp${message.text}
+      <strong>${message.to}:</strong>&nbsp${message.text}
     </div>
     `);
   });
@@ -134,6 +265,18 @@ const goToChat = () => {
 
           <h2>Escolha a visibilidade:</h2>
           <ul class="visibility">
+            <li class="public selected" data-test="public" onclick="selectVisibility(this, 'público')">
+              <ion-icon name="lock-open" class="icon-unlocked"></ion-icon
+              >Público
+              <div class="container-check" data-test="check">
+                <ion-icon name="checkmark-sharp" class="icon-check"></ion-icon>
+              </div>
+            </li>
+            <li class="private" data-test="private" onclick="selectVisibility(this, 'reservadamente')">
+              <ion-icon name="lock-closed" class="icon-locked"></ion-icon
+              >Reservadamente
+              
+            </li>
           </ul>
         </div>
       </div>
@@ -155,17 +298,19 @@ const goToChat = () => {
         <div class="container-input">
           <input
             type="text"
+            class="input-message"
             placeholder="Escreva aqui..."
             data-test="input-message"
           />
-          <span data-test="recipient"
-            >Enviando para Maria (reservadamente)</span
+          <span data-test="recipient" class="recipient"
+            >Enviando para ${to} (${visibility})</span
           >
         </div>
         <ion-icon
           name="paper-plane-outline"
           class="icon-message"
           data-test="send-message"
+          onclick="sendMessage()"
         ></ion-icon>
       </footer>
     </div>
